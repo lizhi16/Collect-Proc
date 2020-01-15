332cb8d524c463c9a1fd5448a47bd5f0ebf0d8ca
lizhi16 <791873122@qq.com>
2020-01-15 14:09:24 -0500

    Add files via upload

--- genetic-container-attack/variantsCalling-new.sh
+++ genetic-container-attack/variantsCalling-new.sh
@@ -9,7 +9,8 @@ mypicard="/home/picard.jar"
 ##set reference and annotation files
 myrefdir="/home/pipeline/"
 myfasta="/home/pipeline/ref/human_g1k_v37.fasta"
-mydata="/home/pipeline/genetic-data-$3/"
+mydata="/home/pipeline/genetic-data/"
 ##Call variants using Haplotype Caller
 ##To download samtools, please refer: http://samtools.sourceforge.net/
-docker run -v $(pwd):/home/pipeline cloudinsky/gatk:bwa /bin/bash -c "cd $myrefdir && $mygatk --java-options \"-Xmx4g\" HaplotypeCaller -R $myfasta -I $mydata$2/$1 -O $mywd$2-GATK-HC.vcf"
+#docker run -v $(pwd):/home/pipeline cloudinsky/gatk:bwa /bin/bash -c "cd $myrefdir && $mygatk --java-options \"-Xmx4g\" HaplotypeCaller -R $myfasta -I $mydata$2/$1 -ERC GVCF -O $mywd$2-GATK-HC.vcf -L 1:197000000-197500000"
+docker run -v $(pwd):/home/pipeline cloudinsky/gatk:bwa /bin/bash -c "cd $myrefdir && $mygatk --java-options \"-Xmx4g\" HaplotypeCaller -R $myfasta -I $mydata$2/$1 -ERC GVCF -O $mywd$2-GATK-HC.vcf -L 1:119672175-119832175"
